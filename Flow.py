99d5cd50d20744a75f043a39058251a8832842a9
Ubuntu <ubuntu@ip-172-31-17-1.us-west-2.compute.internal>
2019-11-18 10:18:55 +0000

    finished topology part

--- Flow.py
+++ Flow.py
@@ -3,18 +3,25 @@ from mininet.net import Mininet
 from mininet.util import dumpNodeConnections
 from mininet.log import setLogLevel
 
-class SingleSwitchTopo(Topo):
+class ExperimentTopo(Topo):
     "Single switch connected to n hosts."
-    def build(self, n=2):
-        switch = self.addSwitch('s1')
-        # Python's range(N) generates 0..N-1
-        for h in range(n):
-            host = self.addHost('h%s' % (h + 1))
-            self.addLink(host, switch)
+    def build(self):
+        s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
+
+        sender   = self.addHost('sender')
+        attacker = self.addHost('attacker')
+        receiver = self.addHost('receiver')
+
+        self.addLink(sender  , s1, bw=100, delay='1ms',  max_queue_size=10)
+        self.addLink(attacker, s1, bw=100, delay='1ms',  max_queue_size=10)
+        self.addLink(receiver, s2, bw=100, delay='1ms',  max_queue_size=10)
+        self.addLink(s1      , s2, bw=1.5, delay='20ms', max_queue_size=1000)
+        
 
 def simpleTest():
     "Create and test a simple network"
-    topo = SingleSwitchTopo(n=4)
+    topo = ExperimentTopo()
     net = Mininet(topo)
     net.start()
     print "Dumping host connections"
